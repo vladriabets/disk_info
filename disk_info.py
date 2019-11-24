59d826f50fd278398e8663adbb091c578f6edfe1
vladriabets <vladriabets@yandex.ru>
2019-11-24 11:40:02 +0300

    refactor (make functions, change command's arguments, etc.)

--- disk_info.py
+++ disk_info.py
@@ -1,42 +1,50 @@
 import subprocess
+import os
+import json
 
-# open file with a string that contains path to the device
-with open('path_to_device.txt', 'r') as f:
-    device_name = f.read()
-    if device_name.startswith('/dev/'):
-        device_name = device_name[5:]
 
-# run commands and reformat their output
-lsblk = subprocess.check_output(['lsblk', '-b', '-l', '-o', 'NAME,SIZE,TYPE,MOUNTPOINT,FSTYPE'])
-df = subprocess.check_output(['df', '-BM', '--output=source,avail'])
-devices_list = lsblk.decode('utf-8').rstrip().split('\n')
-devices = [i.split() for i in devices_list]
+def read_device_name(path_to_file):
 
-# parse commands output to find information about the device
-mountpoint = False
-for row in devices:
-    if device_name == row[0]:
-        # size in GB is rounded to integer
-        size = int(int(row[1]) / 1073741824)
-        typee = row[2]
-        result = '/dev/{0} {1} {2}G'.format(device_name, typee, size)
-        if len(row) == 4:
-            fstype = row[3]
-            result += ' {}'.format(fstype)
-        elif len(row) == 5:
-            mountpoint = row[3]
-            fstype = row[4]
-            result += ' {0} {1}'.format(mountpoint, fstype)
+    """ Reads device name from a file """
 
-if mountpoint:
-    df = df.decode('utf-8').rstrip().split('\n')
-    spaces = [i.split() for i in df]
-    for space in spaces:
-        if space[0] == '/dev/' + device_name:
-            avail_space = space[1]
-            result = '/dev/{0} {1} {2}G {3} {4} {5}'.format(device_name, typee, size, avail_space, fstype, mountpoint)
+    if os.path.exists(path_to_file):
+        with open(path_to_file, 'r', 1) as f:
+            device_name = f.read()
+            return '/dev/' + device_name
+    else:
+        print('path_to_device.txt does not exists')
+        exit(1)
 
-try:
-    print(result)
-except NameError:
-    print('Disk device is not found')
+
+def give_device_info(device_name):
+
+    """ Takes device name and returns information about it """
+    try:
+        lsblk = json.loads(subprocess.check_output(['lsblk', '-b', '--json', '-o',
+                                                  'NAME,SIZE,TYPE,MOUNTPOINT,FSTYPE', device_name]))
+    except:
+        print('Wrong device name')
+        exit(1)
+
+    device_type = lsblk['blockdevices'][0]['type']
+    size = int(int(lsblk['blockdevices'][0]['size'])/1073741824)
+    result = '{0} {1} {2}GB'.format(device_name, device_type, size)
+
+    if lsblk['blockdevices'][0]['fstype']:
+        fstype = lsblk['blockdevices'][0]['fstype']
+        df = subprocess.check_output(['df', '-BM', '--output=source,avail', device_name])
+        df = df.decode('utf-8').rstrip()
+        space_str = df.split('\n')[1]
+        space = space_str.split()[1]
+        result += ' {0} {1}'.format(space, fstype)
+
+    if lsblk['blockdevices'][0]['mountpoint']:
+        mountpoint = lsblk['blockdevices'][0]['mountpoint']
+        result += ' ' + mountpoint
+
+    return result
+
+
+name = read_device_name('path_to_device.txt')
+device_info = give_device_info(name)
+print(device_info)
