ed04a2e99f02595d006c83b064f0dc7da40a69bb
vladriabets <vladriabets@yandex.ru>
2019-11-24 12:45:57 +0300

    catch subprocess.CalledProcessError

--- disk_info.py
+++ disk_info.py
@@ -19,35 +19,38 @@ def read_device_name(path_to_file):
 def give_device_info(device_name):
 
     """ Takes device name and returns information about it """
+
     try:
         lsblk = json.loads(subprocess.check_output(['lsblk', '-b', '--json', '-o',
-                                                  'NAME,SIZE,TYPE,MOUNTPOINT,FSTYPE', device_name]))
-    except:
+                                                   'NAME,SIZE,TYPE,MOUNTPOINT,FSTYPE', device_name]))
+    except subprocess.CalledProcessError:
         print('Wrong device name')
         exit(1)
-
-    device_type = lsblk['blockdevices'][0]['type']
-    size = int(int(lsblk['blockdevices'][0]['size'])/1073741824)
+    device = lsblk['blockdevices'][0]
+    device_type = device['type']
+    size = int(int(device['size'])/1073741824)
     result = '{0} {1} {2}GB'.format(device_name, device_type, size)
 
-    if lsblk['blockdevices'][0]['fstype']:
-        fstype = lsblk['blockdevices'][0]['fstype']
+    if device['fstype']:
+        fstype = device['fstype']
         df = subprocess.check_output(['df', '-BM', '--output=source,avail', device_name])
         df = df.decode('utf-8').rstrip()
         space_str = df.split('\n')[1]
         space = space_str.split()[1]
         result += ' {0} {1}'.format(space, fstype)
 
-    if lsblk['blockdevices'][0]['mountpoint']:
-        mountpoint = lsblk['blockdevices'][0]['mountpoint']
+    if device['mountpoint']:
+        mountpoint = device['mountpoint']
         result += ' ' + mountpoint
 
     return result
+
 
 def main():
     name = read_device_name('path_to_device.txt')
     device_info = give_device_info(name)
     print(device_info)
+
 
 if __name__ == '__main__':
     main()
